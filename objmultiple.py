5062613d3b2ab07d74b999bdc29176b874f9bd08
akashsmaran <akashsmaran@gmail.com>
2018-12-04 06:57:46 +0530

    Background Process made, Frames also skipped

--- objmultiple.py
+++ objmultiple.py
@@ -4,6 +4,10 @@ import argparse
 import imutils 
 import cv2
 import subprocess
+from threading import Thread
+
+def sound1():
+	subprocess.call("python soundcheck2.py", shell=True)
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v","--video",help="path to the (optional) video file")
@@ -23,6 +27,7 @@ pts = deque(maxlen = args["buffer"])
 counter = 0
 (dX, dY) = (0, 0)
 direction = ""
+count = 0
 
 if not args.get("video", False):
 	camera = cv2.VideoCapture(0)
@@ -35,41 +40,44 @@ while True:
 	
 	if args.get("video") and not grabbed:
 		break
-
-	frame = imutils.resize(frame, width=600)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-	
-	mask = cv2.inRange(hsv, blueLower, blueUpper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask,  None, iterations=2)
-
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-	#print(cnts)
-	center = None
-	key =0
-	if len(cnts)>0:
-		for c in cnts: 
-		#c = max(cnts, key=cv2.contourArea)
-			((x, y), radius) = cv2.minEnclosingCircle(c)
-			M = cv2.moments(c)
-			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+	numFrameToSave = 3
+	if (count % numFrameToSave ==0):
+		frame = imutils.resize(frame, width=600)
+		blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+		hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 		
-			if radius > 10:
-				cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-				cv2.circle(frame, center, 5, (0, 0, 255), -1)
-				pts.appendleft(center)
-				counter = counter+1
+		mask = cv2.inRange(hsv, blueLower, blueUpper)
+		mask = cv2.erode(mask, None, iterations=2)
+		mask = cv2.dilate(mask,  None, iterations=2)
 
-			if(x>56 and x<220 and y>280 and y<395 and radius>10):
-				subprocess.call("python soundcheck2.py", shell=True)		
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+		#print(cnts)
+		center = None
+		key =0
+		if len(cnts)>0:
+			for c in cnts: 
+			#c = max(cnts, key=cv2.contourArea)
+				((x, y), radius) = cv2.minEnclosingCircle(c)
+				M = cv2.moments(c)
+				center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			
+				if radius > 10:
+					cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+					cv2.circle(frame, center, 5, (0, 0, 255), -1)
+					pts.appendleft(center)
+					counter = counter+1
 
-		cv2.imshow("Frame", frame)
-		key = cv2.waitKey(1) & 0xFF
+				if(x>56 and x<220 and y>280 and y<395 and radius>10):
+					bg_thread = Thread(target=sound1)
+					bg_thread.start()	
 
-	
-	if key == ord("q"):
-		break
+			cv2.imshow("Frame", frame)
+			key = cv2.waitKey(1) & 0xFF
+
+		
+		if key == ord("q"):
+			break
+	count += 1
 	
 	"""
 	if len(cnts)>0:
